499afb59cbf9e5b5c6b7d9e971f0470f43f2dfc7
marwanMagdy66 <marwanmagdy826@gmail.com>
2023-05-02 18:24:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 // ini_set('display_errors', 1);marwan
     // ini_set('display_startup_errors', 1);
     // error_reporting(E_ALL);
-    
+    //djfsahiufh
+	//fsakjgnfongjsfnjfjj
+	//
     session_start();
 	
 	require_once("system/config.php");
