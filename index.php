b35b7a0f312d7aaa90f05cb8e7f9780b661f4291
MarwanabdalR <97983231+MarwanabdalR@users.noreply.github.com>
2023-05-03 06:06:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // ini_set('display_errors', 1);marwan
     // ini_set('display_startup_errors', 1);
     // error_reporting(E_ALL);
-    
+    // مرووووووووان بيسمع ع مجدي
     session_start();
 	
 	require_once("system/config.php");
