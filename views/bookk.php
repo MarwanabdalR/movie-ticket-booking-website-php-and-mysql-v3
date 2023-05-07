542e91b6d7529576efb415d4e93ff2084bf9219e
MarwanabdalR <97983231+MarwanabdalR@users.noreply.github.com>
2023-05-07 06:37:53 +0200

    ضيفنا الفرونت اند فورم بتاعة البوكينج
    محطناش حاجا في الهيدر عشان بيبوظ في الموفي ديتالز وبيوقف السيرش جرب تربطة بملف ال css بتاعي فية نفس الاكواد بحيث مفيش حاجا تضرب وربك يستر انا هنام

--- views/bookk.php
+++ views/bookk.php
@@ -1,3 +1,27 @@
+<?php
+
+$dbhost="localhost";
+$dbuser="root";
+$dbpass="";
+$dbname="book_cinema";
+
+$connect= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+
+if(!$connect){
+
+    die("faiiiiiiiled" . mysqli_connect_error());
+
+}
+else{
+    echo "Connection succeeded";
+
+}
+
+
+
+
+
+?>
 <!-- breadcrumb area start -->
 <section class="breadcrumb-area">
     <div class="container">
@@ -15,4 +39,118 @@
 
 
 
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+
+        
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            <div class="layout-page">
+            
+
+            <!-- Content wrapper -->
+            <div class="content-wrapper">
+                <!-- Content -->
+
+                <div class="container-xxl flex-grow-1 container-p-y">
+
+                <!-- Basic Layout -->
+                <div class="row">
+                    <div class="col-xl">
+                    <div class="card mb-4">
+                        <div class="card-header d-flex justify-content-between align-items-center">
+                        <h5 class="mb-0">Movie Booking Form</h5>
+                        </div>
+                        <div class="card-body">
+            
+
+
+                        <form method="post">
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-fullname">Full Name</label>
+                            <input type="text" 
+                            class="form-control" 
+                            name="name" 
+                            id="basic-default-fullname" 
+                            placeholder="John Doe" 
+                            />
+                            </div>
+                            
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-email">Email</label>
+                            <div class="input-group input-group-merge">
+                                <input
+                                type="text"
+                                id="basic-default-email"
+                                class="form-control"
+                                name="email"
+                                placeholder="john.doe"
+                                aria-label="john.doe"
+                                aria-describedby="basic-default-email2"
+                                />
+                                <span class="input-group-text" id="basic-default-email2">@example.com</span>
+                            </div>
+
+                            <div class="form-text">You can use letters, numbers & periods</div>
+                            </div>
+                            
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-phone">Phone No</label>
+                            <input
+                                type="text"
+                                id="basic-default-phone"
+                                name="phone"
+                                class="form-control phone-mask"
+                                placeholder="658 799 8941"
+                            />
+                            </div>
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-company">movie name</label>
+                            <input type="text" 
+                            class="form-control" 
+                            name="movie_name" 
+                            id="basic-default-company" 
+                            placeholder="movie name" />
+                            </div>
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-company">Number of Tickets</label>
+                            <input type="text" 
+                            class="form-control" 
+                            name="num_of_tickets" 
+                            id="basic-default-company" 
+                            placeholder="number of tickets" />
+                            </div>
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-company">Movie Time</label>
+                            <input type="time" class="form-control" name="movie_time" id="basic-default-company" placeholder="mm/dd/yyy" />
+                            </div>
+                            <div class="mb-3">
+                            <label class="form-label" for="basic-default-company">Seat Number</label>
+                            <input type="text" class="form-control" name="seat_number" id="basic-default-company" placeholder="seat number" />
+                            </div>
+                            <button type="submit" class="btn btn-primary" name="done">Book Now</button>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
+                    
+                </div>
+                </div>
+                <!-- / Content -->
+
+                
+
+                <div class="content-backdrop fade"></div>
+            </div>
+            <!-- Content wrapper -->
+            </div>
+            <!-- / Layout page -->
+        </div>
+
+        <!-- Overlay -->
+        <div class="layout-overlay layout-menu-toggle"></div>
+    </div>
+    <!-- / Layout wrapper -->
 
--- views/layout/footer.php
+++ views/layout/footer.php
@@ -69,6 +69,26 @@
 <script src="<?= JS; ?>sweetalert.min.js"></script>
 <script scr="<?= JS; ?>languages.js"></script>
 
+
+<!-- ملفات مجدي -->
+
+<script src="../../public/assets/vendor/js/helpers.js"></script>
+
+<!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+<script src="../../public/assets/js/config.js"></script>
+<!-- build:js assets/vendor/js/core.js -->
+<script src="../assets/vendor/libs/jquery/jquery.js"></script>
+    <script src="../../public/assets/vendor/libs/popper/popper.js"></script>
+    <script src="../../public/assets/vendor/js/bootstrap.js"></script>
+    <script src="../../public/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+
+    <script src="../../public/assets/vendor/js/menu.js"></script>
+
+    <!-- Main JS -->
+    <script src="../../public/assets/js/main.js"></script>
+    <!-- Page JS -->
+    <!-- Place this tag in your head or just before your close body tag. -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
 </body>
 
 </html>
