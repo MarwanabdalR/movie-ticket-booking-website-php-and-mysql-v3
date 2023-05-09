6c231fb2179af3d091e536e3c2ad9c3c74d3c8a7
marwanMagdy66 <marwanmagdy826@gmail.com>
2023-05-09 08:40:00 -0700

    update booking
    اضافه جديده للبايمنت

--- models/BookModel.php
+++ models/BookModel.php
@@ -20,9 +20,14 @@ class BookModel extends Model
               $seat_number=$_POST['seat_number'];
             $food=$_POST['food'];
             $food2=implode(',',$food);
+            $card_num=$_POST['card_number'];
+            $card_holder=$_POST['card_holder'];
+            $expiration_mm=$_POST['month'];
+            $expiration_yy=$_POST['year'];
+            $cvv=$_POST['cvv'];
           
     
-    $query="INSERT INTO `booking` ( `name`, `email`, `phone`, `movie name`, `number of tickets`, `movie time`, `seat number`,`food&drinks`) VALUES ('". $fullname ."','".$email."','".$phone_number."','".$movie_name."','".$number_of_tickets."','" .$movie_time."','".$seat_number."','".$food2."')";
+    $query="INSERT INTO `booking` ( `name`, `email`, `phone`, `movie name`, `number of tickets`, `movie time`, `seat number`,`food&drinks`, `card number`, `card holder`, `expiration mm`, `expiration yy`, `cvv`) VALUES ('". $fullname ."','".$email."','".$phone_number."','".$movie_name."','".$number_of_tickets."','" .$movie_time."','".$seat_number."','".$food2."','".$card_num."','".$card_holder."','".$expiration_mm."','".$expiration_yy."','".$cvv."')";
         
 $result=mysqli_query($this->connection, $query);  
 if(!$result){
